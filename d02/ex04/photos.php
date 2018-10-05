5f9d9985d2d152c754f2dd168a8dcc5b1a0ec0b5
hongfu12321 <hongfu1220@gmail.com>
2018-10-04 17:56:20 -0700

    fix photo.php

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -9,10 +9,7 @@ $url = ("http://" . $input);
 
 $text = file_get_contents($url); 
 preg_match_all('/img src="(.*)"/U', $text, $match);
-print_r($match);
-
-
-
+//print_r($match);
 
 $dir_path = (getcwd() . '/' . $input);
 if (!file_exists($dir_path)) mkdir($dir_path, 0755);
@@ -20,20 +17,15 @@ chdir($dir_path);
 
 foreach ($match[1] as $img)
 {
-	if (!strpos($img, "http://")) $img = $url . $img;
+	if (!strpos($img, "http://")) $img = ($url . $img);
 	$curl = curl_init($img);
 	$file_name = basename($img);
 	$file = fopen($file_name, 'wb');
 	curl_setopt($curl, CURLOPT_FILE, $file);
-	curl_setopt($curl, CURLOPT_HEADER, 0);
+	curl_setopt($curl, CURLOPT_URL, $img);
 	curl_exec($curl);
 	curl_close($curl);
 	fclose($file);
 }
-//chdir($dir_path);
-
-//$ch = crul_init();
-//curl_setopt($ch);
- 
 
 ?>
