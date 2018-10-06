3e90bc1bee227af8324a33ef212ef8d7007a574b
hongfu12321 <hongfu1220@gmail.com>
2018-10-06 15:18:07 -0700

    modified modif

--- d04/ex02/modif.php
+++ d04/ex02/modif.php
@@ -2,8 +2,8 @@
 
 function get_data()
 {
-	if (isset($_POST['login'], $_POST['oldpw'], $_POST['newpw'], $_POST['submit']) &&
-			$_POST['submit'] === "OK")
+	if ($_POST['login'] && $_POST['oldpw'] && $_POST['newpw'] &&
+		$_POST['submit']) && $_POST['submit'] === "OK")
 	{
 		$tab['login'] = $_POST['login'];
 		$tab['oldpw'] = hash('sha512', $_POST['oldpw']);
@@ -12,7 +12,7 @@ function get_data()
 	else
 	{
 		echo "ERROR here\n";
-		exit();
+		return (NULL);
 	}
 	return ($tab);
 }
@@ -24,23 +24,17 @@ if (file_exists($file))
 {
 	$modified = 0;
 	$post_tab = get_data();
-	$member_tab = unserialize(file_get_contents($file));
-//	print_r($member_tab);
-	foreach ($member_tab as $key=>$set)
+	if ($post_tab != NULL)
 	{
-		if ($set['login'] == $post_tab['login'])
-		{
-			if ($set['passwd'] == $post_tab['oldpw'])
-			{
-				$member_tab["$key"]['passwd'] = $post_tab['newpw'];
-				$modified = 1;
-			}
-			else
-			{
-				echo "ERROR\n";
-				exit();
-			}
-		}
+		$member_tab = unserialize(file_get_contents($file));
+		//	print_r($member_tab);
+		foreach ($member_tab as $key=>$set)
+			if ($set['login'] == $post_tab['login'])
+				if ($set['passwd'] == $post_tab['oldpw'])
+				{
+					$member_tab["$key"]['passwd'] = $post_tab['newpw'];
+					$modified = 1;
+				}
 	}
 }
 //print_r($member_tab);
